316c1060cf048bb865a40d2a5bfa97372f08f97d
Pundreek Saurabh Srivastava <pundreeksaurabh@gmail.com>
2020-08-14 11:28:29 +0530

    Add files via upload

---
+++ members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'Raghav Garg',
+        email: 'raghavgarg@outlook.com',
+        status : 'active'
+    },
+    {
+        id: 2 ,
+        name: 'Rahul Dwivedi',
+        email: 'rahuldwivedi@gamil.com',
+        status : 'inactive'
+    },
+    {
+        id : 3,
+        name: 'sshrutika goel',
+        email: 'shubhamkumar@yahoo.com',
+        status: 'active'
+    },
+];
+
+module.exports = members; 
